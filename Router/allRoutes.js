f6e35055e64239804e1aceb0b497a73e028dc835
sophiedannery <dannery.sophie@gmail.com>
2024-04-02 19:11:47 +0200

    Ajout HERO SCENE AUTRE

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
