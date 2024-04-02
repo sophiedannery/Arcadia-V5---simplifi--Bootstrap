f1ad4afa16346ac8757ffbfef1331033b2325428
sophiedannery <dannery.sophie@gmail.com>
2024-04-02 21:16:51 +0200

    Ajout PAGE CONNEXION

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
