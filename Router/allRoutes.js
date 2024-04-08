13c8e17dac06eae4d4c8ec3176a71c5134ac87ce
sophiedannery <dannery.sophie@gmail.com>
2024-04-09 00:32:56 +0200

    Ajout authentification dans le routage

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,16 +2,16 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/contact", "Contact", "/pages/contactAll/contact.html"),
-    new Route("/message-send", "Message envoyé", "/pages/contactAll/message-send.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html", "/js/auth/signin.js"),
-    new Route("/espace-admin", "Espace Administrateur", "/pages/espaces/espace-admin.html"),
-    new Route("/services", "Services", "/pages/servicesAll/services.html", "/js/services.js"),
-    new Route("/carteRestaurantA", "Savana Grill", "/pages/servicesAll/carteRestaurantA.html"),
-    new Route("/carteRestaurantB", "Pizzeria de la Jungle", "/pages/servicesAll/carteRestaurantB.html"),
-    new Route("/carteRestaurantC", "Café Oasis", "/pages/servicesAll/carteRestaurantC.html"),
-    new Route("/habitats", "Habitats", "/pages/habitats.html", "/js/habitats.js"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/contact", "Contact", "/pages/contactAll/contact.html", []),
+    new Route("/message-send", "Message envoyé", "/pages/contactAll/message-send.html", []),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/espace-admin", "Espace Administrateur", ["admin", "veterinaire", "employe"], "/pages/espaces/espace-admin.html"),
+    new Route("/services", "Services", "/pages/servicesAll/services.html", [], "/js/services.js"),
+    new Route("/carteRestaurantA", "Savana Grill", "/pages/servicesAll/carteRestaurantA.html", []),
+    new Route("/carteRestaurantB", "Pizzeria de la Jungle", "/pages/servicesAll/carteRestaurantB.html", []),
+    new Route("/carteRestaurantC", "Café Oasis", "/pages/servicesAll/carteRestaurantC.html", []),
+    new Route("/habitats", "Habitats", "/pages/habitats.html", [], "/js/habitats.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
