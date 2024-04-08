d7c2d4b55fabb23ee722d40639db2a40041f390c
sophiedannery <dannery.sophie@gmail.com>
2024-04-08 22:23:50 +0200

    Ajout des cartes restaurant

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,9 @@ export const allRoutes = [
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/signin", "Connexion", "/pages/signin.html", "/js/signin.js"),
     new Route("/services", "Services", "/pages/services.html", "/js/services.js"),
+    new Route("/carteRestaurantA", "Savana Grill", "/pages/servicesAll/carteRestaurantA.html"),
+    new Route("/carteRestaurantB", "Pizzeria de la Jungle", "/pages/servicesAll/carteRestaurantB.html"),
+    new Route("/carteRestaurantC", "Café Oasis", "/pages/servicesAll/carteRestaurantC.html"),
     new Route("/habitats", "Habitats", "/pages/habitats.html", "/js/habitats.js"),
 ];
 
