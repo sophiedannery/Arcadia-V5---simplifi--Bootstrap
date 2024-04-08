4919ec928cbe9470524c06b79564b696d548be3d
sophiedannery <dannery.sophie@gmail.com>
2024-04-09 01:15:35 +0200

    Ajout Configuration Restaurants (boutons + modal)

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,7 @@ export const allRoutes = [
     new Route("/contact", "Contact", "/pages/contactAll/contact.html", []),
     new Route("/message-send", "Message envoyé", "/pages/contactAll/message-send.html", []),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
-    new Route("/espace-admin", "Espace Administrateur", ["admin", "veterinaire", "employe"], "/pages/espaces/espace-admin.html"),
+    new Route("/espace-admin", "Espace Administrateur", "/pages/espaces/espace-admin.html", ["admin", "veterinaire", "employe"]),
     new Route("/services", "Services", "/pages/servicesAll/services.html", [], "/js/services.js"),
     new Route("/carteRestaurantA", "Savana Grill", "/pages/servicesAll/carteRestaurantA.html", []),
     new Route("/carteRestaurantB", "Pizzeria de la Jungle", "/pages/servicesAll/carteRestaurantB.html", []),
